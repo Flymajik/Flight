221c1f30bc4a41b46f1207f662891952d6313d4f
Senayel islam Dip <109631413+Flymajik@users.noreply.github.com>
2022-10-04 23:42:41 +0600

    Update green.py

--- green.py
+++ green.py
@@ -1,5 +1,30 @@
-import os, sys
+import os, platform
+ 
 try:
-    __import__("Flighti").FlightNX()
-except Exception as e:
-    exit(str(e))
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from mahadif import Flight X
+ 
+        Flight X()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from mahadif32 import Flight X
+ 
+ 
+        Flight X()
